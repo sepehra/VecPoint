076b7a189891852bf100cb2fd8f44d026d5b8288
Sepehr <69404366+sepehra@users.noreply.github.com>
2021-05-08 02:43:55 +0430

    Update VectorFunction.jl

--- VectorFunction.jl
+++ VectorFunction.jl
@@ -1,17 +1,14 @@
-function vector(t)
-    # Change (x(t), y(t), z(t)) values based on your need
-    function x(t)
-        return sin(t) - cos(t)
-    end
+# Change (x(t), y(t), z(t)) values based on your need
+function x(t)
+    return sin(t) - cos(t)
+end
 
-    function y(t)
-        return cos(t) - sin(t)
-    end
+function y(t)
+    return cos(t) - sin(t)
+end
 
-    function z(t)
-        return t
-    end
-    return x(t), y(t), z(t)
+function z(t)
+    return t
 end
 
 function wavefront()
@@ -19,9 +16,9 @@ function wavefront()
     count = 1
     for t in 0:0.1:21 # Determine the range
         write(object, "v ")
-        write(object, string(X(t), " "))
-        write(object, string(Y(t), " "))
-        write(object, string(Z(t), "\n"))
+        write(object, string(x(t), " "))
+        write(object, string(y(t), " "))
+        write(object, string(z(t), "\n"))
         write(object, "p $count\n")
         count += 1
     end
